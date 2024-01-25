0242ddcf8e8a7f7dc5cc159dba00a3c75d2ccc9c
abubakar <abubakar-at-218828204349>
2024-01-26 00:14:17 +0500

    first commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -59,11 +59,11 @@ export async function POST(req: Request) {
 
     const user = {
       clerkId: id,
-      email: 'email_addresses[0].email_address',
-      username: 'username!',
-      firstName: 'first_name',
-      lastName: 'last_name',
-      photo: 'image_url',
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
     }
 
     const newUser = await createUser(user);
